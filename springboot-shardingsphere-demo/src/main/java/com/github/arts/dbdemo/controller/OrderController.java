573ef6cd8d55f4e2f4db63dd9ee1ea82a4f09031
moboxs <liuhh26@sina.com>
2021-03-18 21:55:43 +0800

    sharding

--- springboot-shardingsphere-demo/src/main/java/com/github/arts/dbdemo/controller/OrderController.java
+++ springboot-shardingsphere-demo/src/main/java/com/github/arts/dbdemo/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.github.arts.dbdemo.controller;
 
+import org.apache.shardingsphere.transaction.core.TransactionType;
+import org.apache.shardingsphere.transaction.core.TransactionTypeHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,15 +20,20 @@ public class OrderController {
 
     @RequestMapping(value = "order", method = RequestMethod.GET)
     public Object getOrder(@RequestParam Integer userId, @RequestParam Integer orderId) {
-        try {
-            Connection connection = shardingDataSource.getConnection();
+        TransactionTypeHolder.set(TransactionType.XA);
+        try (Connection connection = shardingDataSource.getConnection()) {
+            connection.setAutoCommit(false);
             String sql = "select * from t_order where user_id = ? and order_id = ?";
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, userId);
             preparedStatement.setInt(2, orderId);
             ResultSet resultSet = preparedStatement.executeQuery();
+            connection.commit();
             while (resultSet.next()) {
-                //
+                System.out.println(resultSet.getString(1));
+                System.out.println(resultSet.getString(2));
+                System.out.println(resultSet.getString(3));
+
             }
         } catch (SQLException e) {
             e.printStackTrace();
